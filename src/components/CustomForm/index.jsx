6d530379ba1b58dd1402ac6a7d942c958f97f1fa
mbyn123 <1832376466@qq.com>
2021-03-26 20:01:23 +0800

    更新

--- src/components/CustomForm/index.jsx
+++ src/components/CustomForm/index.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { Form, Button, Input, Radio, InputNumber, Spin, message, Select } from "antd"
+import { Form, Button, Input, Radio, InputNumber, Spin, message } from "antd"
 import { requestData } from "@/http/api/comm"
 import requestUrl from "@/http/api/requestUrl"
-import Selects from '@/components/Select'
+
 
 const layout = {
     labelCol: { span: 2 },
@@ -18,45 +18,35 @@ class CustomForm extends Component {
                 'Input': '请输入',
                 'TextArea': '请输入',
                 'InputNumber': '请输入',
-                'Radio': '请选择',
-                'Select': '请选择',
-                'SelectComponent': '请选择'
+                'Radio': '请选择'
             },
             loading: false
         }
     }
 
     // 监听props是否变化
-    componentDidUpdate() {
+    componentDidUpdate () {
         if (this.props.config.setFieldValue) {
             this.form.current.setFieldsValue(this.props.config.setFieldValue)
         }
     }
-    onFinish = async (val) => {
-        let key = this.props.config.selectComponent
-
-        if (key && val[key] && Object.prototype.toString.call(val[key]) === '[object Object]') {
-            let value = val[key]
-            delete val[key]
-            val = Object.assign(value, val)
-        }
-
+    onFinish = (e) => {
         if (this.props.onSubmit) {
-            this.props.onSubmit(val)
+            this.props.onSubmit(e)
             return
         }
+        this.uploadData(e)
+    }
 
+    uploadData = async (val) => {
         this.setState({ loading: true })
-        let { queryUrl, editKey, setFieldValue } = this.props.config
+        let { queryUrl } = this.props.config
         let query = {
             url: requestUrl[queryUrl],
             method: 'POST',
             data: val
         }
-        if (editKey) {
-            query.data[editKey] = setFieldValue[editKey]
-        }
-        const { data: res } = await requestData(query).catch(err => err)
+        const {data:res} = await requestData(query).catch(err => err)
         console.log(res)
         if (res.resCode !== 0) {
             message.error(res.message)
@@ -66,8 +56,6 @@ class CustomForm extends Component {
         message.success(res.message)
         this.setState({ loading: false })
     }
-
-
 
     marked = (item) => {
         return this.state.hintType[item.type] + item.label
@@ -83,13 +71,6 @@ class CustomForm extends Component {
             rules = rules.concat(item.rules)
         }
         return rules
-    }
-
-    checkPrice = (rule, value) => {
-        if (!value) {
-            return Promise.reject(new Error('请选择'));
-        }
-        return Promise.resolve();
     }
 
     elemInput = (item) => {
@@ -130,29 +111,6 @@ class CustomForm extends Component {
         )
     }
 
-    elemSelect = (item) => {
-        return (
-            <Form.Item label={item.label} name={item.name} rules={this.rules(item)} key={item.name}>
-                <Select >
-                    {
-                        item.options && item.options.map(elem => {
-                            return <Select.Option value={elem.value} key={elem.value}>{elem.label}</Select.Option>
-                        })
-                    }
-                </Select >
-            </Form.Item>
-        )
-    }
-
-    elemSelectComponent = (item) => {
-
-        return (
-            <Form.Item label={item.label} name={item.name} rules={[{ validator: this.checkPrice }]} key={item.name}>
-                <Selects name={item.name} labelInValue={item.labelInValue} url={item.url}></Selects>
-            </Form.Item>
-        )
-    }
-
     initialize = () => {
         let { formItem } = this.props.config
         if (!formItem || (formItem && formItem.length === 0)) { return false }
@@ -162,13 +120,11 @@ class CustomForm extends Component {
             item.type === 'InputNumber' && FormItemList.push(this.elemInputNumber(item))
             item.type === 'Radio' && FormItemList.push(this.elemRadio(item))
             item.type === 'TextArea' && FormItemList.push(this.elemTextArea(item))
-            item.type === 'Select' && FormItemList.push(this.elemSelect(item))
-            item.type === 'SelectComponent' && FormItemList.push(this.elemSelectComponent(item))
         })
         return FormItemList
     }
 
-    render() {
+    render () {
         let { initialValues } = this.props.config
         return (
             <Spin tip="Loading..." spinning={this.state.loading} size="large">
